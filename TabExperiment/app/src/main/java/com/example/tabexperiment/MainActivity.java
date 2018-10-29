b1843a56de7bc849e0afa89c05c38e2605962969
Spolm <Spooner_lm@hotmail.com>
2018-10-29 10:04:46 -0400

    a

--- TabExperiment/app/src/main/java/com/example/tabexperiment/MainActivity.java
+++ TabExperiment/app/src/main/java/com/example/tabexperiment/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
+                viewPager.setCurrentItem ( tab.getPosition() );
             }
 
             @Override
