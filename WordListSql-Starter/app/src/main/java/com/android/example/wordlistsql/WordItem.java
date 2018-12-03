4635aa09b1825ca94fd8cebf7b5fd7a15b91bafc
Sasha Stella <sasha.ginette@gmail.com>
2018-12-02 23:22:55 -0400

    Ejercicio 7.3

---
+++ WordListSql-Starter/app/src/main/java/com/android/example/wordlistsql/WordItem.java
@@ -0,0 +1,30 @@
+package com.android.example.wordlistsql;
+
+import android.database.Cursor;
+
+public class WordItem {
+
+    private int mId;
+    private String mWord;
+
+    public WordItem() {
+
+    }
+
+    public int getId() {
+        return this.mId;
+    }
+
+    public String getWord() {
+        return this.mWord;
+    }
+
+    public void setId(int id) {
+        this.mId = id;
+    }
+
+    public void setWord(String word) {
+        this.mWord = word;
+    }
+
+}
